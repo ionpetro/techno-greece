84870d2ca260728b842c270ec04f885cd754e814
ionpetro <ionpetro@gmail.com>
2023-01-24 11:30:57 +0200

    Improve scraper

--- pages/api/scraper2.js
+++ pages/api/scraper2.js
@@ -10,21 +10,39 @@ export default async function handler(req, res) {
   try {
     const browser = await puppeteer.launch({ headless: false }); // default is true
     const page = await browser.newPage();
+    await page.setViewport({
+      width: 1290,
+      height: 1300,
+      deviceScaleFactor: 1,
+    });
     await page.goto("https://ra.co/events/gr/all");
     await autoScroll(page);
 
     const result = await page.evaluate(() => {
-      const headings_elements = document.querySelectorAll(
-        "ul .grid > :first-child > div"
+      const list = document.querySelector(
+        "section > div > div > :last-child > div > ul.grid"
       );
-      const headings_array = Array.from(headings_elements);
-      return headings_array.length;
+
+      const dates = list.querySelector(":first-child").children;
+      // remove ads
+      const dates_array = Array.from(dates).filter(
+        (node) => !node.innerHTML.includes("adSlot")
+      );
+
+      // for each date node, I want to retrieve the event info and add
+      // the date as an attribute
+      return dates_array.map((node) => {
+        const dateText = node
+          .querySelector("div > h3 > span")
+          .textContent.replace("/", "");
+        return new Date(`${dateText} 2023`).toString();
+      });
     });
 
     res.status(200).json({ response: result });
     await browser.close();
   } catch (e) {
-    res.status(400).json({ error: "error" });
+    res.status(400).json({ error: e });
     console.log(e);
   }
 }
